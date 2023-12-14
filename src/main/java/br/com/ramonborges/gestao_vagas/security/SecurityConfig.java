dd5bbd7474721a833f816a4524cc672911fc0306
Ramon Borges <ramonboorges@gmail.com>
2023-12-14 18:16:46 -0300

    feat: add configuratin to validate JWT token coming Authorization header

--- src/main/java/br/com/ramonborges/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/ramonborges/gestao_vagas/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package br.com.ramonborges.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter securityFilter;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,7 +23,8 @@ public class SecurityConfig {
                 auth.requestMatchers("/company/").permitAll();
                 auth.requestMatchers("/auth/company").permitAll();
                 auth.anyRequest().authenticated();
-            });
+            })
+            .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
