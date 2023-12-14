e22d7f38f0e0093d5c74f3a11ab58cdf6479faad
Ramon Borges <ramonboorges@gmail.com>
2023-12-14 17:34:51 -0300

    chore: remove unused dependencies

--- src/main/java/br/com/ramonborges/gestao_vagas/modules/company/controller/AuthCompanyController.java
+++ src/main/java/br/com/ramonborges/gestao_vagas/modules/company/controller/AuthCompanyController.java
@@ -4,7 +4,6 @@ import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/br/com/ramonborges/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/ramonborges/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -4,7 +4,6 @@ import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
