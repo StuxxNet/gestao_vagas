4dbab1fb2895e87f1ecd9853aa942d8d374a60d8
ramon.silveira-borges@telekom.com <ramon.silveira-borges@telekom.com>
2023-12-21 13:16:19 -0300

    feat: add role check in the candidate profile route

--- src/main/java/br/com/ramonborges/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/br/com/ramonborges/gestao_vagas/security/SecurityCandidateFilter.java
@@ -1,8 +1,11 @@
 package br.com.ramonborges.gestao_vagas.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,11 +25,11 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
-        SecurityContextHolder.getContext().setAuthentication(null);
-        String header = request.getHeader("Authorization");
 
         if(request.getRequestURI().startsWith("/candidate")) {
+            SecurityContextHolder.getContext().setAuthentication(null);
+            String header = request.getHeader("Authorization");
+
             if(header != null) {
                 var token = this.jwtProvider.validateToken(header);
 
@@ -36,7 +39,15 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
                 }
 
                 request.setAttribute("candidate_id", token.getSubject());
+                var roles = token.getClaim("roles").asList(Object.class);
 
+                var grants = roles.stream()
+                    .map(
+                        role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())
+                    ).toList();
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token.getSubject(), null, grants);
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
         }
 
--- src/main/java/br/com/ramonborges/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/ramonborges/gestao_vagas/security/SecurityFilter.java
@@ -24,11 +24,11 @@ public class SecurityFilter extends OncePerRequestFilter{
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
             throws ServletException, IOException {
-        
-        SecurityContextHolder.getContext().setAuthentication(null);
-        String header = request.getHeader("Authorization");
 
         if(request.getRequestURI().startsWith("/company")) {
+            SecurityContextHolder.getContext().setAuthentication(null);
+            String header = request.getHeader("Authorization");
+            
             if(header != null) {
                 var subjectToken = this.jwtProvider.validateToken(header);
                 if(subjectToken.isEmpty()) {
